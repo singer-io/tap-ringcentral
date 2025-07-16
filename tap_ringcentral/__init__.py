c63b45a652aa5c5083aee82aa26e35fd68d7af23
Tushar Mittal <tushar.mittal@qlik.com>
2025-07-16 19:20:09 +0530

    [SAC-27863] [SAC-27966] - GL - Libraries upgrades, Tap framework replacement, Discovery mode (#17)

--- tap_ringcentral/__init__.py
+++ tap_ringcentral/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import singer
+import sys
 
-import tap_framework
-from tap_framework.state import save_state
 import argparse
 import json
+
+from tap_ringcentral.discover import discover
 
 from tap_ringcentral.client import RingCentralClient
 from tap_ringcentral.streams import AVAILABLE_STREAMS
@@ -13,35 +14,47 @@ from tap_ringcentral.streams import AVAILABLE_STREAMS
 LOGGER = singer.get_logger()  # noqa
 
 
-class RingCentralRunner(tap_framework.Runner):
+class RingCentralRunner:
+    def __init__(self, args, client):
+        self.config = args.config
+        self.state = args.state
+        self.catalog = args.catalog
+        self.client = client
+        self.available_streams = AVAILABLE_STREAMS
+
+    def save_state(self, state):
+        if not state:
+            return
+        LOGGER.info('Updating state.')
+        singer.write_state(state)
+
+    def do_discover(self):
+        LOGGER.info("Starting discovery")
+        catalog = discover()
+        json.dump(catalog.to_dict(), sys.stdout, indent=2)
+        LOGGER.info("Finished discover")
+
+
     # Sync the streams in the order specified in the
     # streams/__init__.py list of AVAILABLE_STREAMS
     def do_sync(self):
         LOGGER.info("Starting sync.")
 
-        streams = self.get_streams_to_replicate()
-        stream_map = {s.NAME: s for s in streams}
-
-        for available_stream in AVAILABLE_STREAMS:
-            if available_stream.NAME not in stream_map:
-                continue
-
-            stream = stream_map[available_stream.NAME]
+        for stream_to_sync in self.catalog.get_selected_streams(self.state):
+            stream_obj = self.available_streams[stream_to_sync.stream](
+                        self.config, self.state, stream_to_sync, self.client
+                    )
             try:
-                stream.state = self.state
-                stream.sync()
-                self.state = stream.state
-            except OSError as e:
-                LOGGER.error(str(e))
-                exit(e.errno)
-
+                stream_obj.state = self.state
+                stream_obj.sync()
+                self.state = stream_obj.state
             except Exception as e:
                 LOGGER.error(str(e))
                 LOGGER.error('Failed to sync endpoint {}, moving on!'
-                             .format(stream.TABLE))
+                             .format(stream_obj.TABLE))
                 raise e
 
-        save_state(self.state)
+        self.save_state(self.state)
 
 
 @singer.utils.handle_top_exception(LOGGER)
@@ -49,20 +62,18 @@ def main():
     args = singer.utils.parse_args(required_config_keys=[
         'client_id',
         'client_secret',
-        'username',
-        'password',
+        'refresh_token',
         'api_url',
         'start_date'
     ])
 
-    client = RingCentralClient(args.config)
+    client = RingCentralClient(args.config, args.config_path)
 
-    runner = RingCentralRunner(
-        args, client, AVAILABLE_STREAMS)
+    runner = RingCentralRunner(args, client)
 
     if args.discover:
         runner.do_discover()
-    else:
+    elif args.catalog:
         runner.do_sync()
 
 
